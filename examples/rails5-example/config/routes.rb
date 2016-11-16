7e0253fb45de58562fdf13c3dd2d53c922907181
Ben Zheng <ben@ekohe.com>
2016-11-16 12:14:29 -0800

    FEATURE #59194 - Upgrade Rails
    - add rails 5 example

---
+++ examples/rails5-example/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resource :session, :controller => :session
+  resource :account, :controller => :account
+
+  match "/oauth/authorize", :via => :get, :to => "authorization#new"
+  match "/oauth/authorize", :via => :post, :to => "authorization#create"
+
+  root :to => 'home#show'
+end
